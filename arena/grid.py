cb4bbd3d496c50528b5d9eac45c23910244422ca
Jason Fiammetta <jason.fiammetta@gmail.com>
2024-04-02 23:21:37 -0400

    Clarified Grid class

--- arena/grid.py
+++ arena/grid.py
@@ -1,26 +1,35 @@
+import random
+
+MAX_STOCK_NUM = 10
 
 class Grid:
+    """
+    An N-dimensional grid, with each location representing a number of stocks owned
+    """
     def __init__(self, stock_list, bots):
         self.grid_size = len(stock_list)
-        self.grid = stock_list.map(lambda x: x.price)
+        self.grid_prices = stock_list.map(lambda x: x.price)
         self.bots = bots
         self.time = 0
 
+    def get_origin(self):
+        return [0] * self.grid_size
+
+    def get_random_loc(self):
+        for i in range(self.grid_size):
+            random.randint(-MAX_STOCK_NUM, MAX_STOCK_NUM)
+
     def calc_spot(self, loc):
         sum = 0
-        for stock in loc:
-            sum += stock.price
+        for i in range(self.grid_size):
+            sum += self.grid_size[i] * loc[i]
         return sum
 
     def step(self):
         self.time += 1
-        for bot in bots:
-            bot.worth = calc_spot(bot.loc)
+        for bot in self.bots:
+            bot.worth = self.calc_spot(bot.loc)
             bot.live()
 
     def move(self, bot, new_loc):
-        cost = calc_cost(bot.loc, new_loc)
-
-
-class Location:
-    def __init__(self, )
+        cost = self.calc_cost(bot.loc, new_loc)
