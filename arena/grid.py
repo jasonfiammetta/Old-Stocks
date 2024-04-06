acb681dab2bea689b41a4df56e61ff1a591953dc
Jason Fiammetta <jason.fiammetta@gmail.com>
2024-04-05 23:50:32 -0400

    Updated grid calculation and test data

--- arena/grid.py
+++ arena/grid.py
@@ -7,11 +7,12 @@ class Grid:
     """
     An N-dimensional grid, with each location representing a number of stocks owned
     """
-    def __init__(self, stock_list, bots):
+    def __init__(self, stock_list, bots, update, time = 0):
         # print(stock_list)
         self.grid_prices = list(map(lambda x: x['price'], stock_list))
         self.bots = bots
-        self.time = 0
+        self.time = time
+        self.update = update
 
     @staticmethod
     def get_origin():
@@ -23,14 +24,23 @@ class Grid:
 
     def calc_spot(self, loc):
         sum = 0
+        print('calc_spot', self.grid_prices, loc)
         for i in range(GRID_SIZE):
             sum += self.grid_prices[i] * loc[i]
         return sum
+    
+    def update_stocks(self):
+        stock_list = self.update(self.time)
+        self.grid_prices = list(map(lambda x: x['price'], stock_list))
 
     def step(self):
         self.time += 1
         for bot in self.bots:
-            bot.act()
+            bot.act(self.grid_prices)
+        
+        self.update_stocks()
+
+        for bot in self.bots:
             value = self.calc_spot(bot.loc)
             bot.live(value)
 
--- arena/main.py
+++ arena/main.py
@@ -6,16 +6,16 @@ csv = '.csv'
 stock_list = [] # derive from csv
 stocks = 11 # len(stockList)
 
-bot_num = 1 # 1000
+bot_num = 2 # 1000
 bots = []
-stock_list = [{ "name": 'SPY',  "price": 500.00 }]
+stock_list = [[{ "name": 'SPY',  "price": 500.00 }], [{ "name": 'SPY',  "price": 501.00 }]]
 initial_money = 10000
 
 for i in range(bot_num):
-    bots.append(Bot())
+    bots.append(Bot(i))
 
 bank = Bank(initial_money, bots)
-grid = Grid(stock_list, bots)
+grid = Grid(stock_list[0], bots, (lambda time: stock_list[time]))
 
 print (grid.stats())
 
