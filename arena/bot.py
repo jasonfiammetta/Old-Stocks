431ab0951598e70e04d1ef6a1b2a8a8b4f8d7f85
Jason Fiammetta <jason.fiammetta@gmail.com>
2024-04-05 23:50:50 -0400

    Added liquid cash separate from life

--- arena/bot.py
+++ arena/bot.py
@@ -6,24 +6,33 @@ COST_OF_LIVING = 1 # Money drain
 DEATH_THRESHOLD = 100
 
 class Bot:
-    def __init__(self, loc = None):
+    def __init__(self, name, loc = None):
+        self.name = name
         self.genes = Genes() # seed?
         # self.loc = loc or Grid.get_random_loc()
-        self.loc = loc or Grid.get_origin()
-        self.life = 1000 # money
+        # self.loc = loc or Grid.get_origin()
+        self.loc = loc or Grid.get_origin() if name % 2 else Grid.get_random_loc()
+        self.life = 10000 # money
+        self.liquid = self.life
         self.age = 0
         self.alive = True
 
-    def act(self):
+        self.buy_or_sell(500.00, self.loc[0]) # Testing
+
+    def act(self, grid_prices):
         # do nothing 
         # or
-        # move to new location
+        # move to new location (buy_or_sell)
         # genes decide
         pass
 
+    def buy_or_sell(self, stock_price, quantity):
+        self.liquid -= stock_price * quantity
+        # self.life -= TRANSACTION_FEE
+
     def live(self, gain):
-        self.life -= COST_OF_LIVING
-        self.life += gain
+        self.liquid -= COST_OF_LIVING
+        self.life = gain + self.liquid
         self.age += 1
         if self.life < DEATH_THRESHOLD:
             self.die()
